1701e43ec6ba49c6b9b2cd82dcdba51f1e39017c
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-07-01 02:34:05 +0400

    -- Pr7504

--- src/Pr7504.java
+++ src/Pr7504.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Pr7504 {
     public static void main(String[] args) {
@@ -11,25 +11,82 @@ public class Pr7504 {
         Rectangle r1 = new Rectangle(new Point(in.nextInt(), in.nextInt()),
                 new Point(in.nextInt(), in.nextInt()));
 
+        Rectangle r2 = new Rectangle(new Point(in.nextInt(), in.nextInt()),
+                new Point(in.nextInt(), in.nextInt()));
+
+        Rectangle r3 = new Rectangle(new Point(in.nextInt(), in.nextInt()),
+                new Point(in.nextInt(), in.nextInt()));
+
+        Set<Point> points = new HashSet<Point>();
+
+        for(int i=r1.left(); i<r1.right(); i++){
+            for(int j=r1.top(); j<r1.bottom(); j++){
+                points.add(new Point(i, j));
+            }
+        }
+
+        for(int i=r2.left(); i<r2.right(); i++){
+            for(int j=r2.top(); j<r2.bottom(); j++){
+                points.add(new Point(i, j));
+            }
+        }
+
+        for(int i=r3.left(); i<r3.right(); i++){
+            for(int j=r3.top(); j<r3.bottom(); j++){
+                points.add(new Point(i, j));
+            }
+        }
+
+        System.out.println(points.size());
     }
 }
 
 class Rectangle {
-    Point a;
-    Point b;
+    public Point p1;
+    public Point p2;
 
-    public Rectangle(Point a, Point b) {
-        this.a = a;
-        this.b = b;
+    public Rectangle(Point p1, Point p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public int left(){
+        return Math.min(p1.x, p2.x);
+    }
+
+    public int right(){
+        return Math.max(p1.x, p2.x);
+    }
+
+    public int top(){
+        return Math.min(p1.y, p2.y);
+    }
+
+    public int bottom(){
+        return Math.max(p1.y, p2.y);
     }
 }
 
 class Point {
-    int x;
-    int y;
+    public int x;
+    public int y;
 
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Point)) return false;
+        if(o == null) return false;
+        Point that = (Point) o;
+        return (that.x == this.x) && (that.y == this.y);
+    }
+
+    @Override
+    public int hashCode() {
+        return x << 16 + y;
     }
 }
