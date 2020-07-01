c98372e745b038b44650d8dbe9dcc5efff637507
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-07-01 15:52:21 +0400

    -- Pr9018

--- src/Pr9018.java
+++ src/Pr9018.java
@@ -2,36 +2,30 @@ import java.util.Scanner;
 
 public class Pr9018 {
     public static void main(String[] args) {
-        Test a = new Test();
+        Pr9018 a = new Pr9018();
         a.calc();
     }
-}
 
-class Test {
     public void calc(){
         Scanner in = new Scanner(System.in);
-        long a = in.nextLong();
-        long b = in.nextLong();
-        long n = in.nextLong();
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int n = in.nextInt();
 
-        long ans = binarySeach(1,1000000000,n,a,b);
+        int ans = binarySeach(1,1000000000,n,a,b);
 
-        long maxx = Math.max(a,b);
-
-        while(maxx >= 0){
-            long temp = ans;
+        while(true){
+            int temp = ans;
             ans = binarySeach(1,ans-1,n,a,b);
 
             if(ans == -1){
                 System.out.println(binarySeach(1,temp,n,a,b));
                 break;
             }
-
-            maxx--;
         }
     }
 
-    public long ebob(long a, long b){
+    public int ebob(int a, int b){
         while(b != 0){
             a = a % b;
             a = b - a + (b = a);
@@ -40,17 +34,17 @@ class Test {
         return a;
     }
 
-    public long ekob(long a, long b){
+    public int ekob(int a, int b){
         return a * b / ebob(a,b);
     }
 
-    public long numberofDivisors(long left, long right, long div){
-        long last = right / div * div;
+    public int numberofDivisors(int left, int right, int div){
+        int last = right / div * div;
         return (last - div) / div + 1;
     }
 
-    public long binarySeach(long left, long right, long n, long a, long b) {
-        long mid = left + (right - left) / 2;
+    public int binarySeach(int left, int right, int n, int a, int b) {
+        int mid = left + (right - left) / 2;
         //System.out.println(mid + " " + left + " " + right + " " + n + " " + a + " " + b);
         //System.out.println(numberofDivisors(1, mid, a) + numberofDivisors(1, mid, b) - numberofDivisors(1, mid, ekob(a, b)));
         if(left > right){
