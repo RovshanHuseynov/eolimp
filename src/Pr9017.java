e4a504f190e46aabbc8b97b54fd40df36c30eab4
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-07-02 10:24:41 +0400

    -- Pr9017

---
+++ src/Pr9017.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Pr9017 {
+    public static void main(String[] args) {
+        //Pr9017 a = new Pr9017();
+        Test a = new Test();
+        a.calc();
+    }
+}
+
+class Test {
+    public void calc(){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int q = in.nextInt();
+        int x;
+        HashMap<Integer, Integer> cnt = new HashMap<>();
+
+        for(int i=0; i<n; i++){
+            x = in.nextInt();
+
+            if(cnt.get(x) == null){
+                cnt.put(x, 1);
+            }
+            else{
+                cnt.put(x, cnt.get(x) + 1);
+            }
+        }
+
+        for(int i=0; i<q; i++){
+            x = in.nextInt();
+
+            if(cnt.get(x) == null){
+                System.out.println(0);
+            }
+            else{
+                System.out.println(cnt.get(x));
+            }
+        }
+    }
+}
