958d85af70631942ab02914904f992cf7e587506
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-07-02 04:30:34 +0400

    -- Pr8538

--- src/Pr8538.java
+++ src/Pr8538.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Pr8538 {
     public static void main(String[] args) {
         Pr8538 a = new Pr8538();
+        a.calc();
+    }
+
+    public void calc(){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int cnt = 0;
+        while(n > 1){
+            cnt++;
+
+            if(n % 3 == 0){
+                n /= 3;
+            }
+            else{
+                n--;
+            }
+
+            //System.out.println(n);
+        }
+
+        System.out.println(cnt);
     }
 }
