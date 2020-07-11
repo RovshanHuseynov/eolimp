376bda46bc3407a5410b32c3360d9832e800b4dd
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-07-11 14:52:39 +0400

    -- Pr10045

--- src/linkedList/LinkedListt.java
+++ src/linkedList/LinkedListt.java
@@ -13,7 +13,7 @@ public class LinkedListt {
 
     Node head;
 
-    public void insert(int val){
+    public void addLast(int val){
         Node newNode = new Node(val);
 
         if(head == null){
@@ -28,6 +28,49 @@ public class LinkedListt {
 
             cur.next = newNode;
         }
+    }
+
+    public void addFirst(int val){
+        Node newNode = new Node(val);
+
+        if(head == null){
+            head = newNode;
+        }
+        else{
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    public boolean removeFirst(){
+        if(head == null){
+            return false;
+        }
+
+        head = head.next;
+        return true;
+    }
+
+    public boolean removeLast(){
+        if(head == null){
+            return false;
+        }
+
+        int size = size();
+        int cnt = 1;
+        Node cur = head;
+
+        while(cur.next != null){
+            if(cnt + 1 == size){
+                cur.next = null;
+                break;
+            }
+
+            cnt++;
+            cur = cur.next;
+        }
+
+        return true;
     }
 
     public void print(){
@@ -52,5 +95,47 @@ public class LinkedListt {
         else{
             System.out.print(" " + cur.val);
         }
+    }
+
+    public boolean isEmpty(){
+        return head == null;
+    }
+
+    public int size(){
+        if(head == null){
+            return 0;
+        }
+
+        int cnt = 0;
+        Node cur = head;
+        while(cur.next != null){
+            cnt++;
+            cur = cur.next;
+        }
+
+        return cnt + 1;
+    }
+
+    public int getTail(){
+        if(head == null){
+            return 0;
+        }
+
+        Node cur = head;
+
+        while(true){
+            if(cur.next == null){
+                return cur.val;
+            }
+            cur = cur.next;
+        }
+    }
+
+    public int getHead(){
+        if(head == null){
+            return 0;
+        }
+
+        return head.val;
     }
 }
--- src/linkedList/Pr10041.java
+++ src/linkedList/Pr10041.java
@@ -3,12 +3,19 @@ package linkedList;
 public class Pr10041 {
     public static void main(String[] args) {
         LinkedListt ll = new LinkedListt();
-        ll.insert(5);
-        ll.insert(6);
-        ll.insert(1);
-        ll.insert(3);
+        ll.addLast(5);
+        ll.addLast(6);
+        ll.addLast(1);
+        ll.addLast(3);
+        ll.addLast(10);
+        ll.addFirst(10);
+        //ll.removeFirst();
+        //ll.removeLast();
+        //System.out.println(ll.getHead());
+        //System.out.println(ll.getTail());
 
         ll.print();
-        ll.printReverseRecursion(ll.head);
+        System.out.println(ll.size());
+        System.out.println(ll.isEmpty());
     }
 }
