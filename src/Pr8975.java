1eaded6c29a9be254d68dc8cf49f1d71916fba28
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-07-21 18:41:45 +0400

    -- Pr8975

---
+++ src/Pr8975.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Pr8975 {
+    public static void main(String[] args) {
+        Test7 a = new Test7();
+        a.calc();
+    }
+}
+
+class Test7 {
+    public void calc(){
+        Scanner in = new Scanner(System.in);
+        int [] arr = new int[202];
+        int n = in.nextInt();
+        int [] x = new int[n];
+
+        for(int i=0; i<n; i++){
+            x[i] = in.nextInt();
+            arr[x[i]+100]++;
+        }
+
+        int maxx = 0;
+        int cnt = 0;
+        for(int i=0; i<202; i++){
+            if(arr[i] > maxx){
+                maxx = arr[i];
+            }
+        }
+        System.out.println(maxx);
+
+        for(int i=0; i<n; i++){
+            if(arr[x[i]+100] == maxx){
+                cnt++;
+                if(cnt > 1){
+                    System.out.print(" ");
+                }
+                System.out.print(x[i]);
+                arr[x[i]+100] = 0;
+            }
+        }
+
+        System.out.println();
+    }
+}
