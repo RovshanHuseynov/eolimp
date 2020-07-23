5d16d25c0c93d944b7e1a8c99a1c7c331648be25
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-07-23 13:05:29 +0400

    -- Pr8323

--- src/Pr8323.java
+++ src/Pr8323.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
-public class Pr8222 {
+public class Pr8323 {
     public static void main(String[] args) {
-        Pr8222 a = new Pr8222();
+        Pr8323 a = new Pr8323();
         a.calc();
     }
 
@@ -12,7 +12,9 @@ public class Pr8222 {
         int i = in.nextInt();
         int j = in.nextInt();
 
-        System.out.println(j-i+1);
-        System.out.println(s.substring(i-1,j));
+        System.out.println(j-i+2);
+        for(int a=i; a<j; a++){
+            System.out.println(s.substring(i-1,a));
+        }
     }
 }
