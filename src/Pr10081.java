655a739fcb574857508ea58146b707a2623ebf33
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-07-24 11:25:46 +0400

    -- Pr10081

---
+++ src/Pr10081.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+
+public class Pr10081 {
+    public static void main(String[] args) {
+        Pr10081 a = new Pr10081();
+        a.calc();
+    }
+
+    int [][] arr;
+    int ans,n,m,row,column,limit;
+
+    public void calc(){
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        m = in.nextInt();
+        ans = 1;
+        arr = new int[n+1][m+1];
+        row = 1;
+        column = m;
+        limit = 0;
+
+        while(ans <= n * m) {
+            goDown();
+            if(ans == n * m + 1){
+                break;
+            }
+            row--;
+            column--;
+
+            goLeft();
+            if(ans == n * m + 1){
+                break;
+            }
+            column++;
+            row--;
+
+            goUp();
+            if(ans == n * m + 1){
+                break;
+            }
+            row++;
+            column++;
+            limit++;
+
+            goRight();
+            if(ans == n * m + 1){
+                break;
+            }
+            column--;
+            row++;
+        }
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<m; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println(arr[i][m]);
+        }
+    }
+
+    public void goRight(){
+        while(column <= m - limit) {
+            arr[row][column++] = (ans++);
+        }
+    }
+
+    public void goDown(){
+        while(row <= n - limit) {
+            arr[row++][column] = (ans++);
+        }
+    }
+
+    public void goLeft(){
+        while(column > limit) {
+            arr[row][column--] = (ans++);
+        }
+    }
+
+    public void goUp(){
+        while(row > limit) {
+            arr[row--][column] = (ans++);
+        }
+    }
+}
