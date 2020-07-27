b48a9bbdf35c20fc67bf5b08c0d967d85b5927b7
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-07-27 21:16:56 +0400

    -- Pr9546

---
+++ src/Pr9546.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Pr9546 {
+    public static void main(String[] args) {
+        Test1 a = new Test1();
+        a.calc();
+    }
+}
+
+class Test1 {
+    public void calc(){
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        int n = s.length();
+        int cur;
+        int [] arr = {2,22,222,3,33,333,4,44,444,5,55,555,6,66,666,7,77,777,7777,8,88,888,9,99,999,9999};
+
+        for(int i=0; i<n; i++){
+            if(s.charAt(i) == ' ')
+                System.out.print(0);
+            else
+                System.out.print(arr[(int)s.charAt(i) - 65]);
+
+            if(i == n-1)
+                System.out.println();
+            else
+                System.out.print(" ");
+        }
+
+    }
+}
