24533d87d49a35bdad6497439a44c5efbc008848
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-07-27 16:52:39 +0400

    -- Pr8255

---
+++ src/Pr8255.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Pr8255 {
+    public static void main(String[] args) {
+        Test10 a = new Test10();
+        a.calc();
+    }
+}
+
+class Test10 {
+    public void calc(){
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int s = in.nextInt();
+        rec(n, s, "");
+    }
+
+    public void rec(int n, int s, String cur){
+        if(n > 0 && 9 * n < s){
+            return;
+        }
+
+        if(n == 0 && s == 0){
+            System.out.println(cur);
+            return;
+        }
+
+        if(n == 0 || s < 0){
+            return;
+        }
+
+
+        if(cur.length() > 0) rec(n-1, s-0, cur + "0");
+        rec(n-1, s-1, cur + "1");
+        rec(n-1, s-2, cur + "2");
+        rec(n-1, s-3, cur + "3");
+        rec(n-1, s-4, cur + "4");
+        rec(n-1, s-5, cur + "5");
+        rec(n-1, s-6, cur + "6");
+        rec(n-1, s-7, cur + "7");
+        rec(n-1, s-8, cur + "8");
+        rec(n-1, s-9, cur + "9");
+    }
+
+    public int sum(String s){
+        int sum = 0;
+
+        for(int i=0; i<s.length(); i++){
+            sum += (int)s.charAt(i) - 48;
+        }
+
+        return sum;
+    }
+}
