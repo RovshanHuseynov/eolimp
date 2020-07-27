1c678b53f36a34c3bda8329a1dc0267980382deb
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-07-27 13:04:56 +0400

    -- Pr8226

---
+++ src/Pr8226.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Pr8226 {
+    public static void main(String[] args) {
+        Test3 a = new Test3();
+        a.calc();
+    }
+}
+
+class Test3 {
+    public void calc(){
+        Scanner in = new Scanner(System.in);
+        String p = in.nextLine();
+        String t = in.nextLine();
+        int ans = 0;
+        boolean flag = false;
+        StringBuilder s = new StringBuilder();
+
+        for(int i=0; i<t.length()-p.length(); i++){
+            flag = true;
+            for(int j=0; j<p.length(); j++){
+                s.append(p.charAt(j));
+
+                if(p.charAt(j) != t.charAt(i+j)){
+                    flag = false;
+                    break;
+                }
+            }
+
+            if(flag){
+                ans = i + 1;
+                break;
+            }
+        }
+
+        System.out.println(s);
+        System.out.println(ans);
+    }
+}
