373e1d733d54df4bbaa463fa89f6ebd8ddfba943
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-07-28 13:19:09 +0400

    -- Pr8361

--- src/Pr8361.java
+++ src/Pr8361.java
@@ -2,45 +2,86 @@ import java.util.Scanner;
 
 public class Pr8361 {
     public static void main(String[] args) {
-        Test a = new Test();
+        Pr8361 a = new Pr8361();
         a.calc();
     }
-}
 
-class Test {
     public void calc(){
         Scanner in = new Scanner(System.in);
         String s = in.nextLine();
         int n = s.length();
-        int cnt = 0;
-        int dis = -100;
-        boolean left = false;
-        boolean ans = false;
+        boolean [][] a = new boolean[101][101];
+        int row = 50, column = 50, cnt = 0;
+        a[row][column] = true;
+        String direction = "right";
+        boolean success = false;
 
         for(int i=0; i<n; i++){
-            if(s.charAt(i) == 'L') left = true;
-            else if(s.charAt(i) == 'R') left = false;
-            else if(left){
-                cnt++;
-                dis--;
+            if(s.charAt(i) == 'L'){
+                switch (direction){
+                    case "right":
+                        direction = "up";
+                        break;
+                    case "left":
+                        direction = "down";
+                        break;
+                    case "up":
+                        direction = "left";
+                        break;
+                    case "down":
+                        direction = "right";
+                        break;
+                }
             }
-            else{
-                cnt++;
-                dis++;
+            else if(s.charAt(i) == 'R'){
+                switch (direction){
+                    case "right":
+                        direction = "down";
+                        break;
+                    case "left":
+                        direction = "up";
+                        break;
+                    case "up":
+                        direction = "right";
+                        break;
+                    case "down":
+                        direction = "left";
+                        break;
+                }
             }
+            else if(s.charAt(i) == 'S'){
+                cnt++;
 
-            if(dis == 0){
-                ans = true;
-                break;
-            }
-            else if(dis == -100){
-                dis = 0;
+                switch (direction){
+                    case "right":
+                        column++;
+                        break;
+                    case "left":
+                        column--;
+                        break;
+                    case "up":
+                        row--;
+                        break;
+                    case "down":
+                        row++;
+                        break;
+                }
+
+                if(a[row][column]){
+                    success = true;
+                    break;
+                }
+                else{
+                    a[row][column] = true;
+                }
             }
         }
 
-        if(ans)
-            System.out.println(cnt+1);
-        else
+        if(success){
+            System.out.println(cnt);
+        }
+        else{
             System.out.println(-1);
+        }
     }
 }
