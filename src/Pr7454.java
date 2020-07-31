c329d89edbfe1ea664cf0306c7036662e8d659db
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-08-01 03:05:45 +0400

    -- Pr7454

---
+++ src/Pr7454.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Pr7454 {
+    public static void main(String[] args) {
+        Test1 a = new Test1();
+    }
+}
+
+class Test1 {
+    public void calc(){
+        Scanner in = new Scanner(System.in);
+    }
+}
