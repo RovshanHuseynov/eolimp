0108168451cccce5a564f6265d390347ba0b5f44
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-08-01 12:48:25 +0400

    -- Pr7454

--- src/Pr7454.java
+++ src/Pr7454.java
@@ -2,12 +2,10 @@ import java.util.Scanner;
 
 public class Pr7454 {
     public static void main(String[] args) {
-        Test1 a = new Test1();
+        Pr7454 a = new Pr7454();
         a.calc();
     }
-}
 
-class Test1 {
     public void calc(){
         Scanner in = new Scanner(System.in);
         long p = in.nextLong();
