700b1dd97b3d230c9003fd7d196e0fb0a076117a
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-08-04 15:44:28 +0400

    -- Pr1685

--- src/Pr1685.java
+++ src/Pr1685.java
@@ -8,14 +8,70 @@ public class Pr1685 {
 }
 
 class Test {
+    boolean [] used;
+    boolean [][] a;
+    int n;
     public void calc(){
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
+        n = in.nextInt();
         Point [] p = new Point[n];
         for(int i=0; i<n; i++){
             p[i] = new Point(in.nextInt(), in.nextInt());
         }
 
+        a = new boolean[n][n];
+        used = new boolean[n];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i != j && !a[i][j]) {
+                    if ((Math.abs(p[i].x - p[j].x) == 1 && p[i].y == p[j].y) ||
+                            (Math.abs(p[i].y - p[j].y) == 1 && p[i].x == p[j].x) ||
+                            (Math.abs(p[i].x - p[j].x) == 1 && Math.abs(p[i].y - p[j].y) == 1)) {
+                        a[i][j] = true;
+                        a[j][i] = true;
+                    }
+                }
+            }
+        }
+
+        int cnt = 0;
+        boolean flag;
+
+        for(int i=0; i<n; i++) {
+            used[i] = true;
+            dfs(i);
+            flag = true;
+            cnt++;
+
+            for(int j=0; j<n; j++) {
+                if(!used[j]){
+                    flag = false;
+                    break;
+                }
+            }
+
+            if(flag) break;
+        }
+
+        /*for(int i=0; i<n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(a[i][j]) System.out.print(1 + " ");
+                else System.out.print(0 + " ");
+            }
+            System.out.println();
+        }*/
+
+        System.out.println(cnt);
+    }
+
+    public void dfs(int i){
+        used[i] = true;
+
+        for(int j=0; j<n; j++){
+            if(a[i][j] && !used[j]){
+                dfs(j);
+            }
+        }
     }
 
     class Point {
