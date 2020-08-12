12f8b9e617532d3dd6bbbe2d3a771e5b68070d9b
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-08-12 16:08:12 +0400

    -- Pr5471

---
+++ src/Pr5471.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Pr5471 {
+    public static void main(String[] args) {
+        Test a = new Test();
+        a.calc();
+    }
+}
+
+class Test {
+    int n;
+    boolean [] used;
+    double [] dis;
+    int[][] arr;
+    private final double MAX = 10.0;
+
+    public void calc(){
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        used = new boolean[n+1];
+        arr = new int[n+1][n+1];
+        dis = new double[n+1];
+
+        for(int i=0; i<=n; i++){
+            dis[i] = MAX;
+        }
+
+        int m = in.nextInt();
+        int u,v,w;
+
+        for(int i=0; i<m; i++){
+            u = in.nextInt();
+            v = in.nextInt();
+            w = in.nextInt();
+
+            arr[u][v] = w;
+        }
+
+        dis[1] = 0;
+        dij(1);
+
+        for(int i=2; i<=n; i++){
+            System.out.printf("%8f\n", dis[i]);
+        }
+    }
+
+    public void dij(int from){
+        used[from] = true;
+
+        for(int i=1; i<=n; i++){
+            if(arr[from][i] != 0 && !used[i] && dis[i] > dis[from] + (double) 1 / (double)arr[from][i]){
+                dis[i] = dis[from] + (double) 1 / (double)arr[from][i];
+            }
+        }
+    }
+}
