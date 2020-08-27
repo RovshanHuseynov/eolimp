65ea27a907ba435db39c79d755dea06606eccb6f
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-08-27 15:21:37 +0400

    -- Pr2770

--- src/Pr2770.java
+++ src/Pr2770.java
@@ -2,13 +2,17 @@ import java.util.Scanner;
 
 public class Pr2770 {
     public static void main(String[] args) {
-        Test a = new Test();
+        Pr2770 a = new Pr2770();
         a.calc();
     }
-}
 
-class Test {
     public void calc(){
         Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+
+        if(n == k){
+            System.out.println(1);
+        }
     }
 }
