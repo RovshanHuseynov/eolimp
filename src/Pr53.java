c9e7634f0855193e352748f13edd173fb7782e69
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-08-29 19:04:01 +0400

    -- Pr53

--- src/Pr53.java
+++ src/Pr53.java
@@ -10,7 +10,7 @@ public class Pr53 {
 }
 
 class Test {
-    int n, m, start, finish;
+    int n, m, start, finish, cur_i, cur_j, cur_position;
     Node [][] a;
     Node [][] used;
     Node [][] dis;
@@ -30,12 +30,101 @@ class Test {
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 a[i][j] = new Node();
+                used[i][j] = new Node(0,0);
+                dis[i][j] = new Node(0,0);
+                if(cnt == start){
+                    cur_i = i;
+                    cur_j = j;
+                    cur_position = 1; // left
+                }
                 a[i][j].first = cnt++;
+                if(cnt == start){
+                    cur_i = i;
+                    cur_j = j;
+                    cur_position = 2; // right
+                }
                 a[i][j].second = cnt++;
-                System.out.print(a[i][j].first + "/" + a[i][j].second + "   ");
+                //System.out.print(a[i][j].first + "/" + a[i][j].second + "   ");
             }
-            System.out.println();
+            //System.out.println();
         }
+
+        bfs();
+
+
+    }
+
+    public void bfs(){
+        q.add(start);
+        int top;
+
+        while(!q.isEmpty()){
+            top = q.remove();
+
+            for(int i=0; i<n; i++){
+                for(int j=0; j<m; j++){
+                    if(top == a[i][j].first){
+                        cur_i = i;
+                        cur_j = j;
+                        cur_position = 1; // left
+                        i = n;
+                        j = m;
+                    }
+                    else if(top == a[i][j].second){
+                        cur_i = i;
+                        cur_j = j;
+                        cur_position = 2; // right
+                        i = n;
+                        j = m;
+                    }
+                }
+            }
+
+            if(cur_position == 1)
+                used[cur_i][cur_j].first = 1;
+            else if(cur_position == 2)
+                used[cur_i][cur_j].second = 1;
+
+
+            if(finish == top){
+                if(cur_position == 1)
+                    System.out.println(dis[cur_i][cur_j].first);
+                else if(cur_position == 2)
+                    System.out.println(dis[cur_i][cur_j].second);
+
+                break;
+            }
+
+            if(cur_position == 1) {
+                if(cur_j > 1 && used[cur_i][cur_j-1].second == 0){
+                    q.add(a[cur_i][cur_j-1].second);
+                    dis[cur_i][cur_j-1].second = dis[cur_i][cur_j].first + 1;
+                }
+                if(used[cur_i][cur_j].second == 0){
+                    q.add(a[cur_i][cur_j].second);
+                    dis[cur_i][cur_j].second = dis[cur_i][cur_j].first + 1;
+                }
+                if(cur_i > 1 && used[cur_i-1][cur_j].second == 0){
+                    q.add(a[cur_i-1][cur_j].second);
+                    dis[cur_i-1][cur_j].second = dis[cur_i][cur_j].first + 1;
+                }
+            }
+            else if(cur_position == 2) {
+                if(used[cur_i][cur_j].first == 0){
+                    q.add(a[cur_i][cur_j].first);
+                    dis[cur_i][cur_j].first = dis[cur_i][cur_j].second + 1;
+                }
+                if(cur_j < (m-1) && used[cur_i][cur_j+1].first == 0){
+                    q.add(a[cur_i][cur_j+1].first);
+                    dis[cur_i][cur_j+1].first = dis[cur_i][cur_j].second + 1;
+                }
+                if(cur_i < (n-1) && used[cur_i+1][cur_j].first == 0){
+                    q.add(a[cur_i+1][cur_j].first);
+                    dis[cur_i+1][cur_j].first = dis[cur_i][cur_j].second + 1;
+                }
+            }
+        }
+
     }
 
     class Node {
